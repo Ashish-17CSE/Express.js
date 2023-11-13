585fa33dc99cc475b148995a38492a4c6ea1e42e
ashish.as28121998@gmail.com <ashish.as28121998@gmail.com>
2023-11-14 01:20:32 +0530

    First Express JS Code !

--- 1. Started Express/app.js
+++ 1. Started Express/app.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use((req, res, next)=> {
     console.log("In The Middleware !");
-    next();
+    next(); //Allows the request to continue to the next middleware in line.
 })
 
-app.use('/',(req, res, next) => {
+app.use((req, res, next) => {
     console.log("In another Middleware!");
     res.send('<h1> Hello from Express ! <h1>');
 });
